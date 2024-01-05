f68abc328c202f5ad6aedaa90c06edf93f8a921d
geekelo <efueliht12@gmail.com>
2024-01-05 16:45:57 +0100

    🟢 Add Admin origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://localhost:3001'] # Add your frontend origin(s)
+    origins ['https://oilchem-admin-prod.onrender.com', 'http://localhost:3000', 'http://localhost:3001'] # Add your frontend origin(s)
 
     resource '/api/*',
       headers: :any,
